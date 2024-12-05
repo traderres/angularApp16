677ded20e1540b4aa121f0884e453df40379d078
Adam Resnick <adam@rocky.localdomain>
2024-12-04 20:00:43 -0500

    Updated the maven profiles and added scripts to automate building and pushing-up to AWS ECR

---
+++ scripts/m.bat
@@ -0,0 +1,78 @@
+@Echo off
+REM ##########################################################################################
+REM # Filename:  m.bat
+REM #
+REM # Purpose:   Build and Deploy the webapp to the Amazon ECR
+REM #
+REM # Design
+REM #  1) Generate an AWS ECR token
+REM #  2) Split the token into 2 parts (because it's larger than the 1024 char limit for the "for" and "set" windows commands
+REM #  3) Combine the tokens and pass them into maven
+REM #
+REM # Assumptions
+REM #  1) The user has run aws configure and has an access key and secret key
+REM #  2) The user has tee commands here:  C:\Program Files\Git\usr\bin\tee.exe
+REM #      NOTE:  Git comes with them but does not include it in the PATH by default
+REM #      Usually, Git for Windows would have these installed here:  C:\Program Files\Git\usr\bin
+REM ##########################################################################################
+
+IF "%1"=="dev1" (
+  SET buildProfile=-PbuildAndPushSyncService
+  SET envName=dev1
+  GOTO Compile
+)
+IF "%1"=="dev2" (
+  SET buildProfile=-PbuildAndPushSyncService
+  SET envName=dev2
+  GOTO Compile
+)
+IF "%1"=="demo" (
+  SET buildProfile=-PbuildAndPushSyncService
+  SET envName=demo
+  GOTO Compile
+)
+GOTO Invalid
+
+
+
+:Compile
+  REM This command grabs the login token and writes it to a temporary file
+  ECHO Refreshing AWS ECR token...
+  aws ecr get-login-password --region us-east-1 > %TEMP%\ecr_pass.txt
+
+  REM This for loop splits the output from the file into two variables, pass1 and pass2
+  REM The token exceeded 1800 characters at the time of creating this script
+  setlocal enabledelayedexpansion
+  for /F "tokens=* delims=" %%a in (C:\tmp\ecr_pass.txt) do (
+    SET str=%%a
+    SET pass1=!str:~0,1000!
+    SET pass2=!str:~1000,1000!
+  )
+
+  del /q %TEMP%\ecr_pass.txt
+  del /q %TEMP%\maven-output.txt
+
+
+  REM This command builds and pushes the image to the appropriate repository
+  REM The two password variables from the previous step are combined into the one long password string
+  REM Both the username and password for the from and to authorizations must be provided in the following command
+  CALL mvn -Djib.to.auth.username=AWS -Djib.to.auth.password=%pass1%%pass2% -Djib.from.auth.username=AWS -Djib.from.auth.password=%pass1%%pass2% -Daws.env.name=%envName% clean package %buildProfile% | "C:\Program Files\Git\usr\bin\tee" %TEMP%\maven-output.txt
+
+  REM Look for the "BUILD SUCCESS" string in the output file
+  findstr /n /c:"BUILD SUCCESS"  %TEMP%\maven-output.txt
+
+  IF %ERRORLEVEL% == 0 goto End1
+  GOTO Compile_failed
+
+
+:Compile_failed
+  echo.
+  echo.
+  echo Your branch failed to compile. It has not been pushed to the %1 AWS environment.
+GOTO End1
+
+:Invalid
+  ECHO Invalid argument provided. Options are 'dev1', 'dev2', or 'demo'.
+GOTO End1
+
+:End1
---
+++ scripts/m.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+##########################################################################################################
+# Filename:  mclean.sh
+##########################################################################################################
+# Purpose:
+#   Build and push the Web App image to the Amazon ECR
+#
+# Design
+#   1. Get the user's AWS token in a variable
+#   2. Pass-in the AWS token into the mvn command (so that the jib maven plugin uses it to push up images to our Amazon ECR)
+#   3. Run the mvn command with these profiles:  "prod" and "buildAndPushImage"
+#      unix> mvn -Djib.to.auth.username=AWS -Djib.to.auth.password=$PASSWORD -Djib.from.auth.username=AWS -Djib.from.auth.password=$PASSWORD -Daws.env.name=dev clean package -Pprod -PbuildAndPushImage
+#
+# Usage:
+#    unix> chmod u+x scripts/m.sh               # Make the script executable
+#    unix> scripts/m.sh dev1
+##########################################################################################################
+
+if [ "$1" != "dev1" ] && [ "$1" != "dev2" ] && [ "$1" != "demo" ]; then
+  echo -e "USAGE: m.sh <env name>    Possible values are 'dev1', 'dev2', or 'demo'."
+  exit 1
+fi
+
+
+# Refresh the user's AWS token and store it in a variable
+echo -e "\nRefreshing your AWS token with this command"
+echo -e "\taws ecr get-login-password --region us-east-1\n"
+export PASSWORD=`aws ecr get-login-password --region us-east-1`
+
+# Run the maven profile to build and deploy the image to Amazon's ECR
+echo -e "Running Maven with these profiles:  prod and buildAndPushSyncService  env is $1"
+echo -e "\tmvn -Djib.to.auth.username=AWS -Djib.to.auth.password=\$PASSWORD -Djib.from.auth.username=AWS -Djib.from.auth.password=\$PASSWORD -Daws.env.name=$1  clean package -PbuildAndPushImage\n"
+mvn -Djib.to.auth.username=AWS -Djib.to.auth.password=$PASSWORD -Djib.from.auth.username=AWS -Djib.from.auth.password=$PASSWORD -Daws.env.name=$1  clean package -PbuildAndPushImage | tee /tmp/output.txt
+
+# Check if the build worked successfully
+grep "BUILD SUCCESS" /tmp/output.txt
+
+if [ $? -ne 0 ]; then
+    # The last command had a problem.
+    echo -e "\nCritical Error -- the code did not compile"
+    exit 1
+fi
+
+echo "Successfully built and pushed the image up to the ECR"
+exit 0
